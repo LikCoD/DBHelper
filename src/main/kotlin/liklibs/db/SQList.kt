02de531e939fecd047fbbbea02918fa7652fd3a8
LikDan <likdan.company@gmail.com>
2022-01-29 11:40:12 +0300

    Fix bug

--- src/main/kotlin/liklibs/db/SQList.kt
+++ src/main/kotlin/liklibs/db/SQList.kt
@@ -11,7 +11,7 @@ class SQList<E : Any>(
     dbCredentialsFileName: String,
     kClass: KClass<E>,
     serialization: KSerializer<List<E>>,
-    private val list: MutableList<E> = mutableListOf()
+    private val list: MutableList<E> = mutableListOf(),
 ) : MutableList<E> by list {
     val utils: TableUtils<E>
 
@@ -21,53 +21,45 @@ class SQList<E : Any>(
         list.addAll(utils.sync())
     }
 
-    override fun add(element: E): Boolean {
-        utils.insert(list, element)
+    override fun add(element: E): Boolean =
+        list.add(element).also {
+            utils.insert(list, element)
+        }
 
-        return list.add(element)
-    }
+    override fun add(index: Int, element: E) =
+        list.add(index, element).also {
+            utils.insert(list, element)
+        }
 
-    override fun add(index: Int, element: E) {
-        utils.insert(list, element)
+    override fun addAll(index: Int, elements: Collection<E>): Boolean =
+        list.addAll(index, elements).also {
+            utils.insert(list, elements)
+        }
 
-        return list.add(index, element)
-    }
+    override fun addAll(elements: Collection<E>): Boolean =
+        list.addAll(elements).also {
+            utils.insert(list, elements)
+        }
 
-    override fun addAll(index: Int, elements: Collection<E>): Boolean {
-        utils.insert(list, elements)
+    override fun clear() =
+        list.clear().also {
+            utils.delete(list, list)
+        }
 
-        return list.addAll(index, elements)
-    }
+    override fun remove(element: E): Boolean =
+        list.remove(element).also {
+            utils.delete(list, element)
+        }
 
-    override fun addAll(elements: Collection<E>): Boolean {
-        utils.insert(list, elements)
+    override fun removeAll(elements: Collection<E>): Boolean =
+        list.removeAll(elements).also {
+            utils.delete(list, elements)
+        }
 
-        return list.addAll(elements)
-    }
-
-    override fun clear() {
-        utils.delete(list, list)
-
-        return list.clear()
-    }
-
-    override fun remove(element: E): Boolean {
-        utils.delete(list, element)
-
-        return list.remove(element)
-    }
-
-    override fun removeAll(elements: Collection<E>): Boolean {
-        utils.delete(list, elements)
-
-        return list.removeAll(elements)
-    }
-
-    override fun removeAt(index: Int): E {
-        utils.delete(list, list[index])
-
-        return list.removeAt(index)
-    }
+    override fun removeAt(index: Int): E =
+        list.removeAt(index).also {
+            utils.delete(list, list[index])
+        }
 
     override fun set(index: Int, element: E): E {
         return list.set(index, element)
