454a9cef155c093e6dc847d2fb0fc54ea639c682
LikDan <likdan.company@gmail.com>
2022-01-29 14:31:02 +0300

    Update README.MD

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ publishing {
             register<MavenPublication>("gpr") {
                 groupId = "liklibs.db"
                 artifactId = "db-helper"
-                version = "0.1.2"
+                version = "0.1.3"
 
                 from(components["java"])
             }
